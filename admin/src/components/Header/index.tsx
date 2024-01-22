6faa5d6fb06a6ffebea962419ea2588ea63c2cc0
Yuri Cruger <aoddrewiid@gmail.com>
2024-01-22 15:37:03 -0300

    refactor: remove input from header

--- admin/src/components/Header/index.tsx
+++ admin/src/components/Header/index.tsx
@@ -48,15 +48,7 @@ export function Header() {
   return (
     <header className="bg-blackBGHeader flex h-screen flex-col gap-5 p-8 text-white">
       <h2 className="text-3xl font-bold">MARKET</h2>
-      <InputGroup>
-        <div className="relative">
-          <Input placeholder="Search" className="pl-9" />
-          <SearchIcon
-            size={18}
-            className="absolute bottom-[10px] left-3 text-muted-foreground"
-          />
-        </div>
-      </InputGroup>
+
       <nav>
         <NavItem title="Home" />
         <NavItem title="Dashboard" />
