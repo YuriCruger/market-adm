20100beaef5c5cfba7ba51384f42739cbf3da088
Yuri Cruger <aoddrewiid@gmail.com>
2024-01-22 13:51:53 -0300

    fix: remove arrow icon to NavItem component

--- admin/src/components/Header/NavItem.tsx
+++ admin/src/components/Header/NavItem.tsx
@@ -10,7 +10,6 @@ export function NavItem({ title }: NavItemProps) {
     <div className="easy-in-out hover:bg-grayHover flex cursor-pointer items-center p-3 transition duration-200">
       {NAV_ICON[title as keyof typeof NAV_ICON]}
       <h3 className="ml-3 font-bold">{title}</h3>
-      <ArrowRightIcon className="ml-auto" />
     </div>
   );
 }
