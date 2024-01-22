a80ac7c4b900d530da1d09c8686274e47de6b66e
Yuri Cruger <aoddrewiid@gmail.com>
2024-01-22 15:35:12 -0300

    feat: add Home page and redirect to home after login

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Login() {
       .then((result) => {
         dispatch(setUser(result.user));
 
-        router.push("/dashboard");
+        router.push("/home");
       })
       .catch((error) => {
         console.error("Error signing in with Google:", error);
@@ -58,7 +58,7 @@ export default function Login() {
 
     setTimeout(() => {
       setIsLoading(false);
-      router.push("/dashboard");
+      router.push("/home");
     }, 2000);
   };
 
