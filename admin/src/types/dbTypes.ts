fd651aa5382dd314597174ea07b2c33d0c99b431
Yuri Cruger <aoddrewiid@gmail.com>
2024-02-12 11:25:33 -0300

    refactor: new type to Prroduct

--- admin/src/types/dbTypes.ts
+++ admin/src/types/dbTypes.ts
@@ -1,24 +1,10 @@
 export interface Product {
-  id: string;
+  id: number;
   name: string;
-  description: string;
   price: number;
   category: string;
-  brand: string;
   quantityInStock: number;
-  reorderLevel: number;
-  supplier: string;
-  manufacturingDate: string;
-  expirationDate: string | null;
-  weight: number;
-  dimensions: {
-    length: number;
-    width: number;
-    height: number;
-  };
-  images: string[];
-  isFeatured: boolean;
-  createdAt: string; // Formato ISO 8601
+  createdAt: Date; // Formato ISO 8601
 }
 
 export interface ProductsList {
